1f07778b80783a7b2bc23f11df8b9b465c89b94f
Pablo <dave@wltcode.com>
2024-11-06 21:59:28 +0200

    modification to the game

--- main.py
+++ main.py
@@ -11,7 +11,12 @@ operators = {
     '/': divide
 }
 
+symbols = ['+', '-', '*', '/']
 operator = input('Which operator will you like to play using? ("+". "-", "*", "/"): ')
+while operator not in symbols:
+    print("***Enter a valid operator*****")
+    print("\n")
+    operator = input('Which operator will you like to play using? ("+". "-", "*", "/"): ')
 
 counter = 0
 
@@ -21,17 +26,23 @@ for symbol in operators:
             total = index + 1
             num1 = randint(1, 100)
             num2 = randint(1, 100)
-            try:
-                Q = int(input(f'What is the answer to {num1} {symbol} {num2}: '))
-            except ValueError:
-                print("*******Enter a number*********")
+            while True:
+                try:
+                    print("\n")
+                    Q = int(input(f'What is the answer to {num1} {symbol} {num2}: '))
+                    break
+                except ValueError:
+                    print("*******Enter a number*********")
+                    print("\n")
+
+            if Q == operators[symbol](num1, num2):
+                counter += 1
+                print("You got it right!!")
+                print(f"Your score: {counter}/{total}")
             else:
-                if Q == operators[symbol](num1, num2):
-                    counter += 1
-                    print("You got it right!!")
-                    print(f"Your score: {counter}/{total}")
-                else:
-                    print("Unfortunately You did not get this round correct, try do better next round")
-                    print(f"Your score: {counter}/{total}")
-                print(f"The right answer was {operators[symbol](num1, num2)}")            
-                print("\n")
+                print("Unfortunately You did not get this round correct, try do better next round")
+                print(f"Your score: {counter}/{total}")
+            print(f"The right answer was {operators[symbol](num1, num2)}")            
+            print("\n")
+
+print(f"Your final score is: {counter}/{total}")
